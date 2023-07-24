646d7623960e03cc249fc4b0893a84cb5c1452c1
Hyungseock-Yun <bagzein@gmail.com>
2023-07-24 14:13:13 +0900

    - exception 처리

---
+++ src/main/java/com/pokemonreview/api/exception/MyResourceException.java
@@ -0,0 +1,15 @@
+package com.pokemonreview.api.exception;
+
+public abstract class MyResourceException extends RuntimeException{
+
+    public MyResourceException(String message) {
+        super(message);
+    }
+
+    public MyResourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract int getStatusCode();
+
+}
---
+++ src/main/java/com/pokemonreview/api/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.pokemonreview.api.exception;
+
+public class ResourceNotFoundException extends MyResourceException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
+
+}
