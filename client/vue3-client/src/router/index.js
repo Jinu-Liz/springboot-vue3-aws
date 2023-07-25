76241106804b5c6e7983c64e1c77b9a966037ced
hsyoon <hyungseock.yun@aekyung.kr>
2023-07-25 17:55:15 +0900

    - Client 유저 등록 부분 추가

--- client/vue3-client/src/router/index.js
+++ client/vue3-client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore, useAlertStore } from '@/stores';
 import { Home } from '@/views';
 import accountRoutes from './account.routes';
+import usersRoutes from './users.routes';
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,7 @@ export const router = createRouter({
     routes: [
         { path: '/', component: Home },
         { ...accountRoutes },
+        { ...usersRoutes },
         // catch all redirect to home page
         { path: '/:pathMatch(.*)*', redirect: '/' }
     ]
@@ -20,7 +22,7 @@ router.beforeEach(async (to) => {
     alertStore.clear();
 
     // redirect to login page if not logged in and trying to access a restricted page 
-    const publicPages = ['/account/login', ];
+    const publicPages = ['/account/login', '/account/register'];
     const authRequired = !publicPages.includes(to.path);
     const authStore = useAuthStore();
 
